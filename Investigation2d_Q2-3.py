398f5f56306875507a499b944c76b5b3d43de9cd
lucia2p2z <lucia.filippozzi@studenti.unitn.it>
2021-05-13 18:53:39 +0200

    Attempt (first) to the investigations in 2d

---
+++ Investigation2d_Q2-3.py
@@ -0,0 +1,137 @@
+import numpy as np
+import numpy.linalg as la
+import matplotlib.pyplot as plt
+import math
+from mpl_toolkits.mplot3d import Axes3D 
+from time import time
+
+from project2 import jacobi_step_2d, prolongation_op_2d, restriction_op_2d, Au_op_2d
+
+""" 
+Function f of the given linear system 
+"""
+def f_2d(x,y): 
+    return x * math.e**(2*y) 
+
+"""
+Function to plot different functions on the same image. 
+Shows the current plot and wait a little bit to show the next plot with a different colour
+"""
+def plot_solution(ax, x, y, u, n, kmax): 
+    h= 2/n
+    ax.plot_surface(x, y, u) #, label = f"h=2/(2^{math.log2(n)})")
+    #ax.plot_legend()
+    ax.set_title(f"Meshgrid size: h = 2/(2^{math.log2(n)}) = {h:4.4e}")
+    plt.pause(0.8)                       # wait a bit before showing next plot
+    ax.cla()
+
+
+""" 
+Perform a weighted Jacobi in 1 dim until convergence is reached 
+or until we excess the max number of iterations 
+"""
+def weighted_jacobi_2d(uh, fh, omega, tol=1e-8, kmax=100):
+    for k in range(kmax):
+        if jacobi_step_2d(uh, fh, omega) < tol: return k + 1
+    return -1
+
+
+"""
+Perform 1 step of the 2 grid correction scheme for the weighted Jacobi in 2 dim.
+"""
+def two_grid_correction_step_2d(uh, fh, omega, c, tol=1e-8, kmax=100):
+    v = uh.copy()
+    res = 0 
+    jacobi_step_2d(uh, fh, omega)                               # pre - smoothing 
+    rh = fh - Au_op_2d(uh, c)                                         # compute r_h = f_h - A_h * u_h(K)
+    r2h = restriction_op_2d(rh)                              # restriction of residuals
+    e2h = np.zeros_like(r2h)
+    for i in range(10):                                         # ten steps of weighted Jacobi on the coarse grid
+        jacobi_step_2d(e2h, r2h, omega)
+    uh += prolongation_op_2d(e2h) 
+    jacobi_step_2d(uh, fh, omega)                               # post - smoothing 
+    res = la.norm((uh-v).flat, np.inf)
+    return res
+
+"""
+Perform a 2 grid correction for the weighted Jacobi in 1 dim 
+until convergence is reached we excess the max number of iterations.
+"""
+def two_grid_correction_jacobi_2d(uh, fh, omega, c, tol=1e-8, kmax=100):
+    for k in range(kmax):
+        if two_grid_correction_step_2d(uh, fh, omega, c) < tol: return k + 1
+    return -1
+
+"""
+Q2: 
+Investigate the number of iterations needed for the weighted Jacobi method with ω = 2/3
+and the effect on the CPU time, as h decreases.
+"""
+def investigation_weighted_jacobi_2d():
+    omega, kmax = 2/3, 10000000                                         # weight and max number of iterations
+    N = [2**2, 2**3, 2**4, 2**5, 2**6]                               # different values for n
+    print("Weighted Jacobi —— Investigation on the number of iterations and the effect on CPU time")
+    for n in N:
+        print(f"**** N={n} ****")
+        h = 2/n                                                     # amplitute of the intervals: as n increases, h decreases
+        graphics = True                                             # boolean to show the plots
+
+        x = np.linspace(-1, 1, n+1)                                 # uniform partitioning of [-1,1]: grid for x in 1d
+        y = np.linspace(-1, 1, n+1)                                 # uniform partitioning of [-1,1]: grid for y in 1d
+        x,y = np.meshgrid(x,y)
+        
+        fh = f_2d(x,y)
+        uh = np.zeros_like(fh)
+
+        if graphics: 
+            fig = plt.figure()                                      # set up a figure for plotting
+            ax = fig.add_subplot(111, projection="3d")
+
+        tic = time()
+        k = weighted_jacobi_2d(uh, fh, omega, 1e-8, kmax)           # Re-do jacobi method until convergence (or until we reach the max number of iterations) 
+        el = time()-tic
+        if graphics: 
+            plot_solution(ax, x, y, uh, n, kmax)                    # plot the obtained solution from jacobi
+        print(f"Needed {k} Jacobi steps. CPU time: {el:.4f}s.")
+
+"""
+Q3: 
+Implement the two-grid correction scheme, where instead of solving A2h e2h = r2h, 
+you perform ten weighted Jacobi steps on the coarse grid. 
+Investigate the improvement on the number of iterations and on the CPU time, 
+compared to the standard Jacobi iterations you have used above.
+"""
+def investigation_2grid_correction_jacobi_2d():
+    count_plot_u0 = 0
+    omega, kmax = 2/3, 10000000                                         # weight and max number of iterations
+    N = [2**2, 2**3, 2**4, 2**5, 2**6]                                  # different values for n
+    print("\nTwo grid correction scheme —— Investigation on the number of iterations and the effect on CPU time")
+    for n in N:
+        print(f"**** N={n} ****")
+        h = 2/n                                                     # amplitute of the intervals: as n increases, h decreases
+        graphics = True                                             # boolean to show the plots
+
+        x = np.linspace(-1, 1, n+1)                                 # uniform partitioning of [-1,1]: grid for x in 1d
+        y = np.linspace(-1, 1, n+1)                                 # uniform partitioning of [-1,1]: grid for y in 1d
+        x,y = np.meshgrid(x,y)
+        c = x**2 + y**2
+        fh = f_2d(x,y)
+        uh = np.zeros_like(fh)
+
+        if graphics and count_plot_u0 ==0: 
+            fig = plt.figure()                                              # set up a figure for plotting
+            ax = fig.add_subplot(111, projection="3d")
+            count_plot_u0 +=1
+
+        tic = time()
+        k = two_grid_correction_jacobi_2d(uh, fh, omega, c, 1e-8, kmax)     # Jacobi method until convergence (or until we reach the max number of iterations) 
+        el = time()-tic
+        
+        if graphics: 
+            plot_solution(ax, x, y, uh, n, kmax)                            # plot the obtained solution from jacobi
+
+        print(f"Needed {k} two-grid steps. CPU time: {el:.4f}s.")
+
+if __name__ == "__main__":
+    #investigation_weighted_jacobi_2d()
+    investigation_2grid_correction_jacobi_2d()
---
+++ Investigation2d_Q5-7-8.py
@@ -0,0 +1,199 @@
+import numpy as np
+import math 
+import numpy.linalg as la
+import matplotlib.pyplot as plt
+from time import time
+
+from project2 import v_cycle_step_2d, full_mg_2d, Au_op_2d
+
+""" 
+Function f of the given linear system 
+"""
+def f_2d(x,y): 
+    return x * math.e**(2*y) 
+
+"""
+Function to plot different functions on the same image. 
+Shows the current plot and wait a little bit to show the next plot with a different colour
+"""
+def plot_solution_2d(ax, x, y, u, n, kmax): 
+    h= 2/n
+    ax.plot_surface(x, y, u)  #, label = f"h=2/(2^{math.log2(n)})")
+    ax.set_title(f"Meshgrid size: h = 2/(2^{math.log2(n)}) = {h:4.4e}")
+    plt.pause(0.8)                       # wait a bit before showing next plot
+    ax.cla()
+
+
+""" 
+Perform a complete V-cycle method in 2 dim until convergence is reached 
+or until we excess the max number of iterations 
+"""
+def v_cycle_2d(uh, fh, omega, tol=1e-8, kmax=100):
+    for k in range(kmax):
+        if v_cycle_step_2d(uh, fh, omega) < tol: return k + 1
+    return -1
+
+"""
+Q5: 
+Investigate the number of iterations needed for the V-cycle method with ω = 2/3
+and the effect on the CPU time, as h decreases.
+"""
+def investigation_vstep_2d():
+    omega, kmax = 2/3, 10000                                      # weight and max number of iterations
+    count_plot_u0 = 0
+    N = [2**i for i in range(2,10)]                                  # different values for n: N = 2^l for l = 2,3...,9
+    print("V steps —— Investigation on the number of iterations and the effect on CPU time")
+    for n in N:
+        print(f"**** N={n} ****")
+        h = 2/n                                                      # amplitute of the intervals: as n increases, h decreases
+        graphics = True                                             # boolean to show the plots
+
+        x = np.linspace(-1, 1, n+1)                                 # uniform partitioning of [-1,1]: grid for x in 1d
+        y = np.linspace(-1, 1, n+1)                                 # uniform partitioning of [-1,1]: grid for y in 1d
+        x,y = np.meshgrid(x,y)
+        fh = f_2d(x,y)
+        uh = np.zeros_like(fh)
+
+        if graphics: 
+            fig = plt.figure()                                      # set up a figure for plotting
+            ax = fig.add_subplot(111, projection="3d")
+               
+        if graphics and count_plot_u0 ==0:
+            plot_solution_2d(ax, x, y, uh, n, kmax)
+            count_plot_u0 += 1
+
+        tic = time()
+        k = v_cycle_2d(uh, fh, omega, 1e-8, kmax)                        # Re-do jacobi method until convergence (or until we reach the max number of iterations) 
+        el = time()-tic
+        if graphics: 
+            plot_solution_2d(ax, x, y, uh, n, kmax) 
+        print(f"Needed {k} V-cycle steps. CPU time: {el:.4f}s. \n")
+
+"""
+Q7: 
+Investigate the effect on the CPU time for a full multigrid step with ω = 2/3
+and the pseudo and non residuals, as h decreases.
+"""
+def investigation_full_mg_2d():
+    omega, kmax = 2/3, 10000                                      # weight and max number of iterations
+    count_plot_u0 = 0
+    N = [2**i for i in range(2,10)]                                  # different values for n: N = 2^l for l = 2,3...,10
+    print("Full multigrid step —— Investigation on the size of the final pseudo residuals S_h and of the real ones r_h")
+    for n in N:
+        print(f"**** N={n} ****")
+        h = 2/n                                                      # amplitute of the intervals: as n increases, h decreases
+        graphics = False                                             # boolean to show the plots
+
+        x = np.linspace(-1, 1, n+1)                                 # uniform partitioning of [-1,1]: grid for x in 1d
+        y = np.linspace(-1, 1, n+1)                                 # uniform partitioning of [-1,1]: grid for y in 1d
+        x,y = np.meshgrid(x,y)
+        c = x**2 + y**2
+        fh = f_2d(x,y)
+        uh = np.zeros_like(fh)
+
+        if graphics: 
+            fig = plt.figure()                                      # set up a figure for plotting
+            ax = fig.add_subplot(111, projection="3d")
+               
+        if graphics and count_plot_u0 ==0:
+            plot_solution_2d(ax, x, y, uh, n, kmax)
+            count_plot_u0 += 1
+
+        tic = time()
+        pseudo_res = full_mg_2d(uh, fh, omega)   
+        el = time() - tic                     
+        res = la.norm((fh - Au_op_2d(uh, c)).flat, np.inf)
+
+        if graphics: 
+            plot_solution_2d(ax, x, y, uh, n, kmax) 
+            
+        print(f"CPU time: {el:6.4e} s. \nPseudo residuals |s_h^(k+1)| = {pseudo_res:6.4e}. \nResiduals |r_h^(k+1)| = {res:6.4e}. \n")
+
+"""
+Q8: 
+Investigate the number of additional V-cycles needed, and the effect on the total CPU time,
+by using the method full_mg_1d() to provide an initial guess for your V-cycle multigrid
+method.
+Your sequence of grid sizes should include as least N = 2^l, l = 3, . . . , 14.
+"""
+"""
+def investigation_full_mg_addV_cycle_1d():
+    omega, kmax = 2/3, 10000                                      # weight and max number of iterations
+    count_plot_u0 = 0
+    N = [2**i for i in range(3,15)]                                  # different values for n: N = 2^l for l = 2,3...,20
+    print("Full multigrid step —— Investigation on the size of the final pseudo residuals S_h and of the real ones r_h")
+    for n in N:
+        print(f"**** N={n} ****")
+        h = 2/n                                                      # amplitute of the intervals: as n increases, h decreases
+        graphics = True                                             # boolean to show the plots
+
+        x = np.linspace(-1, 1, n+1)                                  # uniform partitioning of [-1,1]: grid for x in 1d
+        fh = f_1d(x)
+        fh[0] = 0
+        fh[-1] = 0
+        uh = np.zeros_like(x)
+        
+        if count_plot_u0 ==0:
+            plot_solution(x, uh, n, kmax)
+            count_plot_u0 += 1
+
+        tic = time()
+        full_mg_1d(uh, fh, omega) 
+        k = v_cycle_1d(uh, fh, omega, tol=1e-8, kmax=100)     
+        el = time() - tic                 
+
+        if graphics: 
+            plot_solution(x, uh, n, kmax) 
+            
+        print(f"Needed {k} additional V-cycles needed. CPU time: {el:.4f}s. \n") 
+"""
+"""
+Q9
+Plot an approximation to the solution u over Closure(Omega) and provide an accurate estimate of max u(x) for x in Omega
+"""
+"""
+def plot_solution_and_max():
+    graphics = True
+    omega, kmax = 2/3, 10000 
+    n = 2**16
+    h = 2/n    
+    x = np.linspace(-1, 1, n+1)                                 # uniform partitioning of [-1,1]: grid for x in 1d
+    fh = f_1d(x)
+    fh[0] = 0
+    fh[-1] = 0 
+    uh = np.zeros_like(x)
+    full_mg_1d(uh, fh, omega) 
+    v_cycle_1d(uh, fh, omega, tol=1e-8, kmax=100)    
+    if graphics:
+        plt.plot(x, uh, color = "red", label = f"u_h^({kmax})")     # plot the final approximated solution u_h^{kmax} where the meshgrid size is h = 2/n
+        plt.legend()
+        plt.title(f"Approximation to the solution u over [-1,1]:")
+
+    approx_max = max(uh)
+    index_max = np.argmax(uh)
+    x_max = x[index_max]
+    print(f"Estimate of max_[-1,1] u(x) = {approx_max}. Xmax = {x_max}")
+
+    err = fh - Au_op(uh)
+    err_x  = err[index_max]
+    print(f"Error = {err_x}")
+    
+    if graphics:
+        plt.scatter(x_max, approx_max)
+        plt.show()
+
+"""
+
+
+
+if __name__ == "__main__":
+    #plot_solution_and_max()
+    investigation_vstep_2d()
+    #investigation_full_mg_2d()
+    """n=2
+    x = np.linspace(-1, 1, n+1)                                 # uniform partitioning of [-1,1]: grid for x in 1d
+    y = np.linspace(-1, 1, n+1)                                 # uniform partitioning of [-1,1]: grid for y in 1d
+    x,y = np.meshgrid(x,y)
+    c = x**2 + y**2
+    print(x)
+    print(c)"""
